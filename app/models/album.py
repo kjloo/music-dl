3ef5047c6fdb507469287cda145215fc9732e166
kjloo <loo.kaleb@gmail.com>
2024-05-11 15:18:45 -1000

    debug

--- app/models/album.py
+++ app/models/album.py
@@ -13,15 +13,18 @@ class Album:
 
     def _download_image(self, album: str, image: str):
         full_link = SPOTIFY_BASE + image
-
+        print(f'Getting Album {album}')
         response = urllib.request.urlopen(full_link)
         html_text = response.read().decode('utf-8')
 
         m = re.compile(r'"(https://i\.scdn\.co\/image\/\w+)"')
         s = m.search(html_text)
         image_url = s.group(1)
-        urllib.request.urlretrieve(
-            image_url, os.path.join(self.dst_dir, album, album + '.jpg'))
+        try:
+            urllib.request.urlretrieve(
+                image_url, os.path.join(self.dst_dir, album, album + '.jpg'))
+        except:
+            print(f'Failed to get {album}')
 
     def download_images(self, songs: List[Song]):
         albums = list(set([(song.album, song.image) for song in songs]))
